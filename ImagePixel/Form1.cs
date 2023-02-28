a797587bd698d046b1cb0ff28b2860d07d737f3d
Serjo85 <bobrosebob07@gmail.com>
2023-02-28 20:57:13 +0300

    List заменён на массив

--- ImagePixel/Form1.cs
+++ ImagePixel/Form1.cs
@@ -11,7 +11,7 @@ namespace ImagePixel
 {
     public partial class Form1 : Form
     {
-        private List<Bitmap> _bitmaps = new List<Bitmap>();
+        private Bitmap[] _bitmaps;
         private Random rnd = new Random(DateTime.Now.Millisecond);
         public Form1()
         {
@@ -20,7 +20,6 @@ namespace ImagePixel
         }
         private void UpdateForm()
         {
-            _bitmaps.Clear();
             pictureBox1.Image = null;
             trackBar1.Value = 0;
             trackBar1.Enabled = false;
@@ -44,70 +43,73 @@ namespace ImagePixel
                 Text = $"Прошедшее время: {sw.Elapsed.ToString().Remove(8)}";
             }
         }
-        
+
 
         private void RunProcessing(Bitmap bitmap, int steps)
         {
             int cnt = bitmap.Height * bitmap.Width;
-      
+
             int[] indexes = new int[cnt];
             for (int i = 0; i < cnt; i++)
                 indexes[i] = i;
-            for (int i=0;i<cnt-1;i++)
+            for (int i = 0; i < cnt - 1; i++)
             {
                 int idx = rnd.Next(i + 1, cnt - 1);
-                int t = indexes[idx];                
+                int t = indexes[idx];
                 indexes[idx] = indexes[i];
                 indexes[i] = t;
             }
 
             int pixelsInStep = cnt / steps;
-       
-            
-            Bitmap currentBitmap = new Bitmap(bitmap.Width, bitmap.Height);
 
+            _bitmaps = new Bitmap[pixelsInStep];
+            Bitmap currentBitmap = new Bitmap(bitmap.Width, bitmap.Height);
             int width = bitmap.Width;
-           
-            unsafe {
+
+            unsafe
+            {
                 for (int i = 1; i < steps; i++)
                 {
+                    BitmapData bitmapData_source =
+                        bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                            ImageLockMode.ReadWrite, bitmap.PixelFormat);
 
-                    BitmapData bitmapData_source = bitmap.LockBits(new Rectangle(0, 0, currentBitmap.Width, currentBitmap.Height), ImageLockMode.ReadWrite, currentBitmap.PixelFormat);
-
-                    BitmapData bitmapData_current = currentBitmap.LockBits(new Rectangle(0, 0, currentBitmap.Width, currentBitmap.Height), ImageLockMode.ReadWrite, currentBitmap.PixelFormat);
-                    int bytesPerPixel = System.Drawing.Bitmap.GetPixelFormatSize(bitmap.PixelFormat)/8;
+                    BitmapData bitmapData_current = currentBitmap.LockBits(
+                        new Rectangle(0, 0, currentBitmap.Width, currentBitmap.Height), ImageLockMode.ReadWrite,
+                        currentBitmap.PixelFormat);
+                    int bytesPerPixel = System.Drawing.Bitmap.GetPixelFormatSize(bitmap.PixelFormat) / 8;
                     int heightInPixels = bitmapData_current.Height;
                     int widthInBytes = bitmapData_current.Width * bytesPerPixel;
 
                     byte* PtrFirstPixel_current = (byte*)bitmapData_current.Scan0;
                     byte* PtrFirstPixel_source = (byte*)bitmapData_source.Scan0;
-                    
 
-                    for (int j = 0; j < pixelsInStep; j++)
+
+                    Parallel.For(0, pixelsInStep - 1, new ParallelOptions() { MaxDegreeOfParallelism = 5 }, (j) =>
                     {
                         int idx = indexes[i * pixelsInStep + j];
                         int x1 = idx % width;
                         int y1 = idx / width;
-                        
+
                         byte* currentLIne_current = PtrFirstPixel_current + (y1 * bitmapData_current.Stride);
                         byte* currentLIne_source = PtrFirstPixel_source + (y1 * bitmapData_source.Stride);
-                        
+
                         currentLIne_current[x1 * bytesPerPixel] = currentLIne_source[x1 * bytesPerPixel];
                         currentLIne_current[x1 * bytesPerPixel + 1] = currentLIne_source[x1 * bytesPerPixel + 1];
                         currentLIne_current[x1 * bytesPerPixel + 2] = currentLIne_source[x1 * bytesPerPixel + 2];
-                    }
-                        
+                    });
+
                     currentBitmap.UnlockBits(bitmapData_current);
                     bitmap.UnlockBits(bitmapData_source);
-                    _bitmaps.Add((Bitmap)currentBitmap.Clone());
+                    _bitmaps[i - 1] = ((Bitmap)currentBitmap.Clone());
                     this.Invoke(new Action(() => { Text = $"{i} %"; }));
                     this.Invoke(new Action(() => { pictureBox1.Image = _bitmaps[trackBar1.Value++]; }));
                 }
             }
-            _bitmaps.Add(bitmap);
+            _bitmaps[steps - 1] = bitmap;
             this.Invoke(new Action(() => { pictureBox1.Image = _bitmaps[trackBar1.Value++]; }));
         }
-        
+
 
         private void trackBar1_Scroll(object sender, EventArgs e) =>
                 pictureBox1.Image = (trackBar1.Value == 0) ? null : _bitmaps[trackBar1.Value - 1];
